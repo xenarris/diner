7b1952b005fd3fece7c025433d95c0ea18f350ae
Jean-Kenneth Antonio <jantonio2@mail.greenriver.edu>
2021-05-27 12:57:55 -0400

    Added classes and templating

--- index.php
+++ index.php
@@ -1,74 +1,53 @@
 <?php
-//This is my controller
 
-//Turn on error reporting
+//Turn on error-reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//start a session
+//Require necessary files
+require_once ('vendor/autoload.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/../config.php');
+//require_once("/home/jantonio/../config.php");
+
+
+//Start a session AFTER the autoload***
 session_start();
 
-//Require autoload file
-require_once('vendor/autoload.php');
-
-//Create an instance of the base class
+//Instantiate Fat-Free and controller
 $f3 = Base::instance();
+$con = new Controller($f3);
 
-//Define a default route
-$f3 -> route('GET /', function() {
-    //display the homepage
-    $view = new Template();
-    echo $view->render('views/home.html');
-    }
-);
+//Define default route
+$f3->route('GET /', function(){
 
-$f3 -> route("GET /breakfast", function () {
-    //display the breakfast page
+    $GLOBALS['con']->home();
+});
+
+$f3->route('GET /breakfast', function(){
+
+    //Display the breakfast page
     $view = new Template();
     echo $view->render('views/breakfast.html');
 });
 
-$f3 -> route("GET /lunch", function () {
-    //display the lunch page
+$f3->route('GET /breakfast/brunch/mothers-day', function(){
+
+    //Display the breakfast page
     $view = new Template();
-    echo $view->render('views/lunch.html');
+    echo $view->render('views/mothers-day-brunch.html');
 });
 
-$f3 -> route("GET|POST /order1", function () {
-    //If the form has been submitted, add the data to session
-    // and send the user to the next order form
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //var_dump($_POST);
-        $_SESSION['food'] = $_POST['food'];
-        $_SESSION['meal'] = $_POST['meal'];
-        header('location: order2');
-    }
-    //display the order1 page
-    $view = new Template();
-    echo $view->render('views/orderForm1.html');
+$f3->route('GET|POST /order1', function(){
+    $GLOBALS['con']->order1();
 });
 
-$f3 -> route("GET|POST /order2", function () {
-    //If the form has been submitted, add the data to session
-    // and send the user to the next order form
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //var_dump($_POST);
-        //Data validation will go here
-
-        $_SESSION['conds'] = implode(", ", $_POST['conds']);
-        header('location: summary');
-    }
-
-    //display the order2 page
-    $view = new Template();
-    echo $view->render('views/orderForm2.html');
+$f3->route('GET|POST /order2', function($f3){
+    $GLOBALS['con']->order2();
 });
 
-$f3 -> route("GET /summary", function () {
-    //display the summary page
-    $view = new Template();
-    echo $view->render('views/summary.html');
+$f3->route('GET /summary', function(){
+    $GLOBALS['con']->summary();
 });
 
-//Run fat free
+//Run Fat-Free
 $f3->run();
